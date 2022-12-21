1012b919ba84b85f4c9fa404ae75dc06e8741e58
Raf <r0463108@student.thomasmore.be>
2022-12-21 19:26:01 +0100

    Hobby.html done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var ropecount = 0;
 // insert image above the first existing image in main-tag
 function insertImageAbove() {
     var topImage = document.querySelector('.hangman')
@@ -6,19 +7,26 @@ function insertImageAbove() {
     image.style.display = 'block';
     image.setAttribute('class', 'hangman offset-1 ps-1 ps-md-2');
     topImage.insertBefore(image, topImage.firstChild);
+    ropecount++;
 }
 
 //listen for scroll event and when at the top of the screen load an image above the first image in the main tag
 window.addEventListener('scroll', () => {
     if (window.scrollY == 0) {
-        insertImageAbove();
+        if (ropecount < 100) {
+            insertImageAbove();
+        } else {
+            window.alert("You reached the top!");
+        }
     }
 })
 //listen for mousewheel movement check if going up and at the top of the screen
 window.addEventListener('wheel', (e) => {
     if (window.scrollY == 0 && e.deltaY < 0) {
-        //var mainImage = document.querySelector('.hangman2');
-        //mainImage.setAttribute('class', 'hangman2');
-        insertImageAbove();
+        if (ropecount < 100) {
+            insertImageAbove();
+        } else {
+            window.alert("You reached the top!");
+        }
     }
 })
